d68f358a57837ebcccd0e6c10cfc46c15055fa75
NinjaO9 <156165726+NinjaO9@users.noreply.github.com>
2026-06-20 13:49:11 -0700

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        if (strs.size() == 1) return {{strs[0]}};
+        unordered_map<string, vector<string>> hgroups;
+        vector<vector<string>> groups;
+
+        for (const string s : strs)
+        {
+            string hash = getFrequencyHash(s);
+            hgroups[hash].push_back(s);
+        }
+
+        for (const pair<string, vector<string>> vs : hgroups)
+        {
+            groups.push_back(vs.second);
+        }
+
+        return groups;
+    }
+
+    string getFrequencyHash(string s)
+    {
+        int arr[26] = {0};
+        string h;
+        for (const char c : s)
+        {
+            arr[c - 'a'] += 1;
+        }
+
+        for (int i = 0; i < 26; i++)
+        {
+            h.push_back((char)('a' + i));
+            h += to_string(arr[i]);
+        }
+        return h;
+    }
+};
+
+
+/*
+
+idea:
+
+add the sum of ASCII characters and group words together that way.
+Then, manually verify that each group should exist.
+
+*/
