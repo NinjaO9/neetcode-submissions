ee1580afbf5f693987cbf29fdb2d8657d1a4e6d7
NinjaO9 <156165726+NinjaO9@users.noreply.github.com>
2026-07-12 22:08:31 -0700

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        s.erase(remove_if(s.begin(), s.end(), [](unsigned char ch){
+            return !isalnum(ch);
+        }), s.end());
+        int p1 = 0;
+        int p2 = s.size() - 1;
+
+        while (p1 < p2)
+        {
+            if (tolower(s[p1]) != tolower(s[p2])) return false;
+            p1++;
+            p2--;
+        }
+        return true;
+    }
+};
